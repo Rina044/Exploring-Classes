bcbb36be8f203bedac20df4fac13eb7f79a4e156
Hfms-beep <kostrovpesgav@yandex.ru>
2024-02-28 06:00:18 +0300

    создание варианта №7 "Пара" пока что без ссылки

---
+++ Exploring classes/PARA/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Para
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PARA_Lib.Class1 ok = new PARA_Lib.Class1();
+
+            ok.StartPara = "10:10";
+            ok.EndPara = "11:40";
+            ok.ChillStart = "11:40";
+            ok.ChillEnd = "11:50";
+            ok.ZvukGolubya = " ";
+        }
+    }
+}
---
+++ Exploring classes/PARA_Lib/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PARA_Lib
+{
+    public class Class1
+    {
+
+        
+        public string StartPara { get; set; } 
+
+
+        public string EndPara { get; set; }
+
+
+        public string ChillStart { get; set; }
+
+
+        public string ChillEnd { get; set; }
+
+
+        public string ZvukGolubya { get; set; }
+       
+        
+    }
+}
